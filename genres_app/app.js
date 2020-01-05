25aeeb749c621478c56c227f51b701984a5a5d79
YONGSU JEONG <jos50275266@gmail.com>
2020-01-06 02:55:14 +0900

    genres_app CRUD

---
+++ genres_app/app.js
@@ -0,0 +1,27 @@
+const morgan = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// routes
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+
+mongoose
+  .connect("mongodb://localhost/movie_theater", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB...", err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
---
+++ genres_app/routes/customers.js
@@ -0,0 +1,73 @@
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+    isGold: { type: Boolean, default: false, required: true },
+    name: { type: String, minlength: 5, maxlength: 50, required: true },
+    phone: { type: String, minlength: 5, maxlength: 50, required: true }
+}))
+
+router.get("/", async (req, res, next) => {
+    const customers = await Customer.find().sort({ name: 1 });
+    res.send(customers);
+});
+
+router.post("/", async (req, res, next) => {
+    const { error } = await validateCustomers(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let customer = new Customer({
+        isGold: req.body.isGold,
+        name: req.body.name,
+        phone: req.body.phone
+    });
+
+    customer = await customer.save();
+    res.send(customer);
+});
+
+router.put("/:id", async (req, res, next) => {
+    const { error } = await validateCustomers(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findOneAndUpdate(
+        req.params.id, {
+        isGold: req.body.isGold,
+        name: req.body.name,
+        phone: req.body.phone
+    },
+        { new: true })
+
+    if (!customer) return res.status(404).send("The customer with the given ID was not found.")
+    res.send(customer);
+});
+
+router.delete("/:id", async (req, res, next) => {
+    const customer = await Customer.findOneAndDelete(req.params.id);
+    if (!customer) return res.status(404).send("The customer with the given ID was not found.");
+
+    res.send(customer);
+});
+
+router.get("/:id", async (req, res, next) => {
+    const customer = await Customer.findById(req.params.id);
+    if (!customer) return res.status(404).send("The customer with the given ID was not found");
+
+    res.send(customer);
+});
+
+async function validateCustomers(customer) {
+    const schema = Joi.object({
+        isGold: Joi.boolean().required(),
+        name: Joi.string().min(5).max(50).required(),
+        phone: Joi.string().min(5).max(50).required()
+    })
+
+    return schema.validate(customer)
+}
+
+
+module.exports = router;
