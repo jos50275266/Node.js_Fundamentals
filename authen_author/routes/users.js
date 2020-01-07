be0a406781a8ea0c6e22629d5d226a9813d99806
YONGSU JEONG <jos50275266@gmail.com>
2020-01-07 21:22:01 +0900

    Delete Comment

--- authen_author/routes/users.js
+++ authen_author/routes/users.js
@@ -12,27 +12,11 @@ router.post('/', async (req, res, next) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    // user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // })
 
-    // 위 코드와 동일
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
     await user.save();
 
-    // 문제점 1: user에게 비밀번호를 send 해주면 안됨.
-    // 첫번째 해결법
-    // res.send({
-    //     name: user.name,
-    //     email: user.email
-    // })
 
-    // 두번째 해결법, Lodash 사용
-    // second property 배열에 원하는 property 이름만
-    // _.pick(user, ['name', 'email'])
-    // joi-password-complexity
 
     res.send(_.pick(user, ['_id', 'name', 'email']));
 })
