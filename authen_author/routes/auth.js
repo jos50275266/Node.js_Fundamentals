77038c5fddd26911bd34707697afcaafe1622886
YONGSU JEONG <jos50275266@gmail.com>
2020-01-07 23:55:57 +0900

    jwt

--- authen_author/routes/auth.js
+++ authen_author/routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const Joi = require("@hapi/joi")
 const { User, validateUser } = require("../models/user")
 const _ = require('lodash');
@@ -17,7 +18,17 @@ router.post('/', async (req, res, next) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    res.send(true);
+    // Instead of true value, we send JWT(JSON Web Token)
+    // Web ex) localstorage: Header, Payload(userID, name, admin: true/false) will be public property,
+    // so we should not include private or important data there, such as password
+    // Verify Signature: Based on the content of the JSON web token along with the secret or private key
+    // , which is only available on server
+    // Header + Payload 를 암호화 한 것을 Verify Signature에 저장하는데 이때, JWT는 secret or private key
+    // 가 필요한데 이것은 서버만 알고있기 때문에 해킹이 불가능하다.
+    // res.send(true);
+
+    const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey'); // (payload, secret or private key)
+    res.send(token);
 })
 
 async function validateAuth(req) {
