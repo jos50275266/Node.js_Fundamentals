99a24f487dca47155b4046300352e06bf60408e9
YONGSU JEONG <jos50275266@gmail.com>
2020-01-11 20:18:59 +0900

    Integration Test Part1:
    Intergration Test를 통해 validateObjectId middleware의 필요성 인지.
    
    처음에:
    ```javascript
            it('should return 404 if invalid is passed, async () => {
               const res = await request(server).get('/api/genres/1);
    	   expect(res.status)toBe(404);
    	})
    ```
    
    위 코드와 같이 /api/genres/:id 에서 :id parameter로 mongodb의 id object
    방식이 아닌 다른 방식의 값을 parameter 값으로 주면 404 error를
    출력해야하는데, Integration Test 이전에 :id parameter가 mongodb id
    object인지 확인하는 logic이 없었기때문에 바로 try...catch 에서 :id 값을
    확인하기 전에 500 error로 넘어갔기 때문에, test를 통해 mongodb id
    object인지 아닌지 확인하는 logic이 필요함을 깨닫고 validateObjectId
    middleware를 추가해 성공적으로 Refactoring을 마쳤다.

--- integration_testing/routes/genres.js
+++ integration_testing/routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require('../middleware/validateObjectId');
 const mongoose = require('mongoose')
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
@@ -38,12 +39,17 @@ router.delete('/:id', [auth, admin], async (req, res) => {
     res.send(genre);
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
     // if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     //     return res.status(404).send('Invalid ID.')
     // }
 
     const genre = await Genre.findById(req.params.id);
+    // 위 if statement이 있기 이전에는 바로 error middleware가 아래 조건문에 도달하기전
+    // try...catch로 error를 잡아서 500 error가 발생했음으로 위와 같이 조건문을 하나 더
+    // 추가 함으로써 404 errror를 출력하게 할 수 있다.
+    // 하지만 현재 위 코드는 다른 라우터 함수에서도 사용될 수 있기 때문에 별도의 함수로 Refactoring 하자.
+    // middleware로 만들어 주자
     if (!genre) return res.status(404).send('The genre with the given ID was not found...');
 
     res.send(genre);
