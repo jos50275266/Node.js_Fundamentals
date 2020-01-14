902778f97dc44d1f501448058c192d6000805f4c
YONGSU JEONG <jos50275266@gmail.com>
2020-01-14 19:28:30 +0900

    Looking Up an Object

--- TDD/tests/integration/returns.test.js
+++ TDD/tests/integration/returns.test.js
@@ -92,6 +92,9 @@ describe('/api/returns', () => {
     });
 
     it('should return 404 if no rental found for this customer/movie', async () => {
-        
-    })
+        await Rental.remove({});
+        const res = await exec();
+        expect(res.status).toBe(404);
+    });
+
 });
