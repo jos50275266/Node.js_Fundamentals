49e3bbdac8e109e9ce9325514c78d2eef913847e
YONGSU JEONG <jos50275266@gmail.com>
2020-01-14 17:13:39 +0900

    Populating the Database

---
+++ TDD/tests/integration/returns.test.js
@@ -0,0 +1,60 @@
+const { Rental } = require('../../models/rental')
+const mongoose = require('mongoose');
+
+
+// POST /api/returns {customerId, movieId}
+
+// Negative Cases
+//Test Case #1. Return 401 if client in not logged in.
+//Test Case #2. Return 400 if customerId is not provided.
+//Test Case #3. Return 400 if movieId is not provided
+//Test Case #4. Return 404 if no rental found for this customer/movie
+//Test Case #5. Return 400 if rental already processed
+
+// Positive Cases
+// Test Case #1. Set the return  date
+// Test Case #2. Calculate the rental fee
+// Test Case #3. Increase the stock
+// Test Case #4. Return the rental
+
+// Test Cases - Let's Brainstorm
+
+describe('/api/returns', () => {
+    let server;
+    let customerId;
+    let movieId;
+    let rental;
+
+    beforeEach(async () => {
+        server = require('../../index');
+
+        customerId = mongoose.Types.ObjectId();
+        movieId = mongoose.Types.ObjectId();
+
+        // DateOut will be automatically set by mongoose.
+        rental = new Rental({
+            customer: {
+                _id: customerId,
+                name: '12345',
+                phone: '12455'
+            },
+            movie: {
+                _id: movieId,
+                title: '12345',
+                dailyRentalRate: 2
+            }
+        })
+        await rental.save();
+    });
+
+    afterEach(async () => {
+        server.close();
+        await Rental.remove({})
+    });
+
+    // One simple Test
+    it('should work!', async () => {
+        await Rental.findById(rental._id);
+        expect(Rental).not.toBeNull();
+    });
+})
--- TDD/x.js
+++ TDD/x.js
@@ -1,2 +1,18 @@
 
 // POST /api/returns {customerId, movieId}
+
+// Negative Cases
+//Test Case #1. Return 401 if client in not logged in.
+//Test Case #2. Return 400 if customerId is not provided.
+//Test Case #3. Return 400 if movieId is not provided
+//Test Case #4. Return 404 if no rental found for this customer/movie
+//Test Case #5. Return 400 if rental already processed
+
+// Positive Cases
+// Test Case #1. Set the return date
+// Test Case #2. Calculate the rental fee
+// Test Case #3. Increase the stock
+// Test Case #4. Return the rental
+
+// Test Cases - Let's Brainstorm
+
