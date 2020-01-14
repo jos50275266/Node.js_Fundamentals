25b2acd90699d9957f8d567b62a505542e5996a3
YONGSU JEONG <jos50275266@gmail.com>
2020-01-14 22:58:48 +0900

    Refactoring the Validation Logic
    const {error} = validator(req.body);
    if(error) return res.status(400).send(error.details[0].message);
    next()
    
    위 코드 부분을 Refactoring해서 하나의 middleware로 만들기
    
    module.exports = (validator) => {
        return (req, res, next) => {
            const {error} = validator(req.body);
    	if(error) return res.status(400).send(error.details[0].message);
    	}
     }

---
+++ TDD/routes/returns_factor_ver.js
@@ -0,0 +1,57 @@
+const validate = require('../middleware/validate')
+const Joi = require('@hapi/joi')
+const moment = require('moment');
+const auth = require('../middleware/auth');
+const { Movie } = require('../models/movie');
+const { Rental } = require('../models/rental');
+const express = require('express');
+const router = express.Router();
+
+// const validate = (validator) => {
+//     return (req, res, next) => {
+//         const { error } = validator(req.body);
+//         if (error) return res.status(400).send(error.details[0].message);
+//         next();
+//     }
+// };
+
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+    // if (!req.body.customerId) return res.status(400).send('customerId not provided');
+    // if (!req.body.movieId) return res.status(400).send('movieId not provided');
+
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('Rental not found...');
+
+    if (rental.dateReturned) return res.status(400).send('Rental already processed...');
+
+    // rental.dateReturned = 1; toBeDefined() test를 통과하는지 간략하게 확인하려고
+    rental.dateReturned = new Date();
+    const rentalDays = moment().diff(rental.dateOut, 'days')
+    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+    await rental.save();
+
+    // Let's use update first approach
+    await Movie.update({ _id: rental.movie._id }, {
+        $inc: { numberInStock: 1 }
+    });
+
+
+    return res.status(200).send(rental);
+});
+
+function validateReturn(req) {
+    const schema = Joi.object({
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required()
+    })
+
+    return schema.validate(genre)
+}
+
+
+
+module.exports = router;
